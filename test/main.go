6f4e72bb02a293bd7ca0e45096df3d43a25fe66f
w-mj <wmj@alphamj.cn>
2023-05-28 22:02:07 +0800

    better test

--- test/main.go
+++ test/main.go
@@ -1,31 +1,27 @@
 package main
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/w-mj/ginj"
-	"sync"
 )
 
-func MyHandler(abc int, def string) (map[string]any, error) {
+// @Ginj: GET /index2?abc&def
+func AnnotateHandler(abc int, def string) (map[string]any, error) {
+	if abc < 100 {
+		return nil, errors.New("abc must grater than 100")
+	}
 	ans := map[string]any{}
 	ans["abc"] = abc
 	ans["def"] = def
 	return ans, nil
 }
 
-// @Ginj: GET /index2?abc&def
-func AnnotateHandler(abc int, def string) (map[string]any, error) {
-	return MyHandler(abc, def)
-}
-
-var wg = sync.WaitGroup{}
-
 //go:generate ginj_gen .
 func StartServer() {
-	r := gin.Default()
+	r := gin.New()
 	j := ginj.New(r)
 	j.LoadAnnotatedRote()
-	j.Route("GET /index?abc&def", MyHandler)
 
 	err := r.Run("127.0.0.1:8729")
 	if err != nil {
--- test/main_test.go
+++ test/main_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
+	"os"
 	"testing"
 	"time"
 )
@@ -31,13 +32,25 @@ func getJson(r *http.Response) map[string]any {
 	return ans
 }
 
-func TestMainFunc(m *testing.T) {
-	a = assert.New(m)
+func TestMain(m *testing.M) {
 	go StartServer()
 	time.Sleep(time.Second * 2)
+	os.Exit(m.Run())
+}
+
+func TestMainFunc(m *testing.T) {
+	a = assert.New(m)
 	ans := get("/index2?abc=123&def=hello")
 	a.Equal(ans.StatusCode, http.StatusOK)
 	j := getJson(ans)
 	a.Equal(j["abc"].(float64), float64(123))
 	a.Equal(j["def"].(string), "hello")
+}
+
+func TestRequestError(m *testing.T) {
+	a = assert.New(m)
+	ans := get("/index2?abc=10&def=abc")
+	a.Equal(ans.StatusCode, http.StatusBadRequest)
+	d, _ := io.ReadAll(ans.Body)
+	m.Log(string(d))
 }
